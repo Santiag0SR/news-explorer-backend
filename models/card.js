354df647f5fbc0b46c8912429a32e574f09d0d6a
Santiago SR <santisachezromero@gmail.com>
2022-06-13 09:35:19 -0300

    add models

---
+++ models/card.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const cardSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String, // the name is a string
+    required: true, // every user has a name, so it's a required field
+  },
+  text: {
+    type: String, // the name is a string
+    required: true, // every user has a name, so it's a required field
+  },
+  date: {
+    type: String, // the name is a string
+    required: true, // every user has a name, so it's a required field
+  },
+  source: {
+    type: String, // the name is a string
+    required: true, // every user has a name, so it's a required field
+  },
+  link: {
+    type: String,
+    required: [true, 'url required'],
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Sorry. You have to enter a valid link',
+    },
+  },
+  image: {
+    type: String,
+    required: [true, 'url required'],
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Sorry. You have to enter a valid link',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
